19099b07325458e96ea4ff0eca9583598d88a702
Pedro Escapalete <pedroescapalete@gmail.com>
2025-09-29 08:55:59 -0300

    Servidor funcionando

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+
+const app = express();
+app.use(express.json());
+
+dotenv.config();
+const serverPort = process.env.PORT || 3001;
+
+app.get('/', (req, res) => {
+  res.send(`🚀 Servidor funcionando...`);
+});
+
+
+app.listen(serverPort, () => {
+  console.log(`🚀 Servidor Funcionando em: http://localhost:${serverPort}`);
+});
